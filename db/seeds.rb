b44c3f7fe177718da72fa462fb72068a3c73f294
Yousuf <yoyousuf67@gmail.com>
2018-01-07 12:46:59 +0530

    Integrated index action for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,7 +20,7 @@ puts"5 skill created"
 		title: "Portfolio item #{portfolio_item}",
 		subtitle: "subtitle you want",
 		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-		main_image: "http://via.placeholder.com/600*400" ,
-		thumb_image: "http://via.placeholder.com/350*200"
+		main_image: "http://via.placeholder.com/600x400" ,
+		thumb_image: "http://via.placeholder.com/350x200"
 	)
 end
