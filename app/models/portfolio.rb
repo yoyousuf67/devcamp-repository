89703ad4d3708919c81e556748c97973232c58f5
Yousuf <yoyousuf67@gmail.com>
2018-01-14 11:40:43 +0530

    implemented scope for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,5 +2,10 @@ class Portfolio < ApplicationRecord
 
 	validates_presence_of :title, :body, :main_image, :thumb_image
 
+	#def self.angular
+	#	where(subtitle: 'Angular')
+	#end
+
+	#scope :ror, ->{where(subtitle: 'Ruby on Rails')}
 end
  
